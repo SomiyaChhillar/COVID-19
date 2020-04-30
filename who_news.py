7f802d379cd20ff8f93d7c48f61f7b355a45d3f4
Rishabh Srivastava <rishabh080598@gmail.com>
2020-04-30 16:37:29 +0530

    All WHO Q&A

--- who_news.py
+++ who_news.py
@@ -3,28 +3,43 @@ from bs4 import BeautifulSoup
 import json
 
 
-URL = "https://www.who.int/news-room/q-a-detail/q-a-coronaviruses"
-r = requests.get(URL) 
+URLs = ["https://www.who.int/news-room/q-a-detail/q-a-coronaviruses", 
+		'https://www.who.int/news-room/q-a-detail/q-a-on-covid-19-and-pregnancy-and-childbirth',
+		'https://www.who.int/news-room/q-a-detail/q-a-on-covid-19-and-breastfeeding',
+		'https://www.who.int/news-room/q-a-detail/q-a-on-covid-19-and-masks',
+		'https://www.who.int/news-room/q-a-detail/q-a-on-covid-19-hiv-and-antiretrovirals',
+		'https://www.who.int/news-room/q-a-detail/q-a-similarities-and-differences-covid-19-and-influenza',
+		'https://www.who.int/news-room/q-a-detail/q-a-on-mass-gatherings-and-covid-19',
+		'https://www.who.int/news-room/q-a-detail/q-a-on-infection-prevention-and-control-for-health-care-workers-caring-for-patients-with-suspected-or-confirmed-2019-ncov',
+		'https://www.who.int/news-room/q-a-detail/q-a-on-smoking-and-covid-19#',
+		'https://www.who.int/news-room/q-a-detail/be-active-during-covid-19',
+		'https://www.who.int/news-room/q-a-detail/malaria-and-the-covid-19-pandemic',
+		'https://www.who.int/news-room/q-a-detail/violence-against-women-during-covid-19',
+		'https://www.who.int/news-room/q-a-detail/contraception-family-planning-and-covid-19'
+		]
+data=[]		
+for URL in URLs:
+	r = requests.get(URL) 
 
-soup = BeautifulSoup(r.content, 'html5lib') 
+	soup = BeautifulSoup(r.content, 'html5lib') 
 
-table = soup.find('div', attrs = {'class':'sf-accordion'})
+	table = soup.find('div', attrs = {'class':'sf-accordion'})
 
-questions = table.findAll('div', attrs = {'class':'sf-accordion__panel'})
+	questions = table.findAll('div', attrs = {'class':'sf-accordion__panel'})
 
-# rows = table.findAll('tr')
+	# rows = table.findAll('tr')
 
-data=[]
-for question in questions:
-	# print(q uestion)
-	q = question.find('div', attrs = {'class':'sf-accordion__trigger-panel'}).a.text.strip()
-	con = question.find('div', attrs = {'class':'sf-accordion__content'}).get_text()
-	a = con.strip()
-	print(q, a, sep='\n')
 
-	obj = {'title':q, 'content':a}
+	for question in questions:
+		# print(q uestion)
+		q = question.find('div', attrs = {'class':'sf-accordion__trigger-panel'}).a.text.strip()
+		con = question.find('div', attrs = {'class':'sf-accordion__content'}).get_text()
+		a = con.strip()
+		print(q, a, sep='\n')
 
-	data.append(obj)
+		obj = {'title':q, 'content':a}
+
+		data.append(obj)
 	
 	
 with open('Data/who.json', 'w') as f:
